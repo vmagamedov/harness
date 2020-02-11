65427a5449c1160b362f90834be6a0d7c2e48108
Vladimir Magamedov <vladimir@magamedov.com>
2020-02-11 12:43:04 +0200

    Added ability to specify language in the command-line parameters

--- src/harness/plugin/main.py
+++ src/harness/plugin/main.py
@@ -10,6 +10,10 @@ from . import python
 from .types import Context, WireIn, WireOut
 
 
+LANGUAGES = {
+    'python': HarnessService.PYTHON,
+}
+
 RENDERER = {
     HarnessService.PYTHON: python.render,
 }
@@ -19,7 +23,7 @@ class ConfigurationError(ValueError):
     pass
 
 
-def process_file(proto_file, response):
+def process_file(proto_file, response, params):
     for config_message in proto_file.message_type:
         if config_message.name == 'Configuration':
             break
@@ -39,16 +43,21 @@ def process_file(proto_file, response):
             else:
                 continue
 
-    language = None
-    for _, option in config_message.options.ListFields():
-        if not isinstance(option, HarnessService):
-            continue
-        language = option.language
-
-    if not language:
-        raise ConfigurationError(
-            '(harness.service).language option is not specified',
-        )
+    if 'language' in params:
+        params_language = params['language']
+        if params_language not in LANGUAGES:
+            raise ConfigurationError(f'Unknown language specified in the '
+                                     f'command-line: {params_language}')
+        else:
+            language = LANGUAGES[params_language]
+    else:
+        language = None
+        for _, option in config_message.options.ListFields():
+            if not isinstance(option, HarnessService):
+                continue
+            language = option.language
+        if not language:
+            raise ConfigurationError('Language is not specified')
 
     ctx = Context(
         proto_file=proto_file.name,
@@ -66,12 +75,17 @@ def main() -> None:
     with os.fdopen(sys.stdin.fileno(), 'rb') as inp:
         request = CodeGeneratorRequest.FromString(inp.read())
 
+    params = {}
+    for pair in request.parameter.split(','):
+        key, _, value = pair.partition('=')
+        params[key] = value
+
     files_to_generate = set(request.file_to_generate)
 
     response = CodeGeneratorResponse()
     for pf in request.proto_file:
         if pf.name in files_to_generate:
-            process_file(pf, response)
+            process_file(pf, response, params)
 
     with os.fdopen(sys.stdout.fileno(), 'wb') as out:
         out.write(response.SerializeToString())
